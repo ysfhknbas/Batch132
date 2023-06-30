4bfaaa374b60b45c4ba1ac416a0b1a6360959c36
Yusuf Bas <yusufhbas@yahoo.com>
2023-06-30 01:26:18 -0400

    first commit

---
+++ src/main/java/day01datatypesvariable/DataTypes01.java
@@ -0,0 +1,29 @@
+package day01datatypesvariable;
+
+
+
+public class DataTypes01 {
+
+    public static void main(String[] args) {
+        // Primitives
+        byte b = 1;
+        short s = 5600;
+        int i = -654;
+        long l= 67576565465465L;
+        float f = 4.56F;
+        double d = 8.654;
+        char c = 'f';
+        boolean t = true;
+        System.out.println(l);
+
+        // Non Primitives
+
+        String str = "ıut876-%";
+
+    }
+
+    }
+
+
+
+
