19fddb9ee16273baeef0b712f9f618e2a21a295e
Yusuf Bas <yusufhbas@yahoo.com>
2023-07-02 21:17:32 -0400

    next commit

---
+++ src/main/java/day04stringmanipulations/Screener01.java
@@ -0,0 +1,21 @@
+package day04stringmanipulations;
+
+import java.util.Scanner;
+
+public class Screener01 {
+    public static void main(String[] args) {
+
+    //getting initials of a name contains first name and last name
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your first and last name");
+        String full = input.nextLine();
+        int spc = full.indexOf(" ");
+        System.out.println(spc);
+        String ilk =full.split(" ")[0];
+        String iki = full.split(" ")[1];
+        System.out.println(ilk);
+        System.out.println(iki);
+
+    }
+}
