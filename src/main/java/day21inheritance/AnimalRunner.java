f783065c90f05a5b6b8d7e85f523a2372189812e
Yusuf Bas <yusufhbas@yahoo.com>
2023-08-19 23:12:35 -0400

    ysf commit

---
+++ src/main/java/day21inheritance/AnimalRunner.java
@@ -0,0 +1,28 @@
+package day21inheritance;
+
+public class AnimalRunner {
+    public static void main(String[] args) {
+
+
+       /* Cat myCat = new Cat();
+        System.out.println(myCat.age);
+        System.out.println(myCat.initial);
+
+        Mammal myCat1 = new Cat();
+        System.out.println(myCat1.age);
+        System.out.println(myCat1.name);
+
+        Animal myCat2 = new Cat();
+        System.out.println(myCat2.age);
+
+        Cat myCat3 = new Cat();
+        System.out.println(myCat3.age);*/
+
+Mammal mammal1 = new Mammal();
+mammal1.drink();
+
+        Animal mammal2 = new Mammal();
+        mammal2.drink();
+
+    }
+}
