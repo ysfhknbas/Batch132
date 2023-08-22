d2b23bc38a170d0490bafb3a03a5a0eb563baf77
Yusuf Bas <yusufhbas@yahoo.com>
2023-08-21 22:47:05 -0400

    ysf commit

---
+++ src/main/java/day21inheritance/Cat.java
@@ -0,0 +1,23 @@
+package day21inheritance;
+
+public class Cat extends Mammal{
+
+public int age = 14;
+public  String name = "B";
+public boolean old = false;
+public double x = 2.3;
+
+
+public Cat()
+{
+    //System.out.println(this.age);
+    //System.out.println(super.age);
+}
+
+public void meaw()
+{
+    System.out.println("Cats meaw...");
+}
+
+
+}
