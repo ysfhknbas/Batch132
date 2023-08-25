1867efbb1c8d986cfa0078981a09e79891968342
Yusuf Bas <yusufhbas@yahoo.com>
2023-08-24 23:43:46 -0400

    ysf commit

---
+++ src/main/java/day21inheritance/Mammal.java
@@ -0,0 +1,21 @@
+package day21inheritance;
+
+public class Mammal extends Animal {
+
+public int age = 6;
+public boolean old = true;
+public char initial ='A';
+
+
+public void drink()
+{
+    System.out.println("Mammals drink...");
+}
+
+public void feed()
+{
+    System.out.println("Mammals feed...");
+}
+
+
+}
