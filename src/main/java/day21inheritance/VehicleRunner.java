cf2ca9328d8a2b005e8eb8546962df043f67ef50
Yusuf Bas <yusufhbas@yahoo.com>
2023-08-26 21:20:42 -0400

    ysf commit

---
+++ src/main/java/day21inheritance/VehicleRunner.java
@@ -0,0 +1,19 @@
+package day21inheritance;
+
+public class VehicleRunner {
+    public static void main(String[] args) {
+        /*Civic myCivic = new Civic(true);
+        myCivic.engine(); //Honda Class
+        myCivic.move();  //Vehicle Class
+        myCivic.ecosystem(); //Civic Class
+        myCivic.brake();  //Honda Class
+        myCivic.price();  //Vehicle Class
+        int hashcode = myCivic.hashCode(); // Object Class
+        System.out.println(hashcode);*/
+
+
+        Civic myCivic = new Civic(true);
+
+
+    }
+}
