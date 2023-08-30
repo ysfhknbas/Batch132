a065b2c3602455fb490d022c51c045c9d2b4f638
Yusuf Bas <yusufhbas@yahoo.com>
2023-08-29 22:24:22 -0400

    ysf commit

---
+++ src/main/java/day22overridingEncapsulation/AnimalRunner.java
@@ -0,0 +1,10 @@
+package day22overridingEncapsulation;
+
+public class AnimalRunner {
+    public static void main(String[] args) {
+       // Cat myCat = new Cat();
+       // myCat.eat();
+
+
+    }
+}
